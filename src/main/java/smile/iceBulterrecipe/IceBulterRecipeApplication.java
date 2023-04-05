b12490091b58e1cd892db63d26fbfa8fa073242a
chaerlo127 <monynony0203@gmail.com>
2023-04-05 12:48:22 +0900

    feat: repo 생성

---
+++ src/main/java/smile/iceBulterrecipe/IceBulterRecipeApplication.java
@@ -0,0 +1,13 @@
+package smile.iceBulterrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceBulterRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceBulterRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smile/iceBulterrecipe/IceBulterRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package smile.iceBulterrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceBulterRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
