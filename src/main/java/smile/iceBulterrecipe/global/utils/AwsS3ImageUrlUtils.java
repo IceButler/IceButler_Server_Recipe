b04c85c57c526fe03cd38a4c42cadd4428502b88
sojungpp <sophie2458@naver.com>
2023-04-25 00:33:52 +0900

    #18 feat: AWS S3 Image 설정

---
+++ src/main/java/smile/iceBulterrecipe/global/utils/AwsS3ImageUrlUtils.java
@@ -0,0 +1,27 @@
+package smile.iceBulterrecipe.global.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class AwsS3ImageUrlUtils {
+
+    public static String bucket;
+    public static String region;
+    public static String profile;
+
+    @Value("${aws.s3.region}")
+    public void setRegion(String value) {
+        region = value;
+    }
+    @Value("${aws.s3.bucket}")
+    public void setBucket(String value) {
+        bucket = value;
+    }
+
+    public static String toUrl(String imageKey) {
+        return "https://"+bucket+".s3."+region+".amazonaws.com/"+imageKey;
+    }
+}
