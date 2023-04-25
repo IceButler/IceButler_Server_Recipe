4fc770722e39a53f49ef8cb1a86cacdb7eabbcdd
chaerlo127 <monynony0203@gmail.com>
2023-04-25 12:19:34 +0900

    #1 feat: recipe Category 생성

---
+++ src/main/java/smile/iceBulterrecipe/global/Constant.java
@@ -0,0 +1,8 @@
+package smile.iceBulterrecipe.global;
+
+public class Constant {
+    public static class RecipeConstant {
+        public static final String FRIDGE_FOOD_RECIPE = "냉장고";
+        public static final String POPULAR_FOOD = "인기";
+    }
+}
