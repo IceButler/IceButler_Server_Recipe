f780e0a7a40ea67547790ee4c1e212a4a845a3b5
sojungpp <sophie2458@naver.com>
2023-04-29 14:27:16 +0900

    #24 fix: 파일 병합으로 잘못된 return 값 해결

--- src/main/java/smile/iceBulterrecipe/recipe/service/RecipeService.java
+++ src/main/java/smile/iceBulterrecipe/recipe/service/RecipeService.java
@@ -1,11 +1,12 @@
 package smile.iceBulterrecipe.recipe.service;
 
 import smile.iceBulterrecipe.global.feign.dto.response.RecipeFridgeFoodListsRes;
+import smile.iceBulterrecipe.recipe.dto.response.BookmarkRes;
 import smile.iceBulterrecipe.recipe.dto.response.RecipeListRes;
 
 public interface RecipeService{
     RecipeListRes getPopularRecipeListsForFridge(Long userIdx, RecipeFridgeFoodListsRes listsRes);
     RecipeListRes getFridgeRecipeLists(Long userIdx, RecipeFridgeFoodListsRes listsRes);
     RecipeListRes getBookmarkRecipes(Long userIdx, RecipeFridgeFoodListsRes fridgeFoodList);
-    void bookmarkRecipe(Long recipeIdx, Long userIdx);
+    BookmarkRes bookmarkRecipe(Long recipeIdx, Long userIdx);
 }
