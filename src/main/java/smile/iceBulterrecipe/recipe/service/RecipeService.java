b1946905dea68a765e7c6ec28311361563ea9d70
sojungpp <sophie2458@naver.com>
2023-05-05 23:38:50 +0900

    #25 feat: 마이 레시피 조회 구현

--- src/main/java/smile/iceBulterrecipe/recipe/service/RecipeService.java
+++ src/main/java/smile/iceBulterrecipe/recipe/service/RecipeService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import smile.iceBulterrecipe.global.feign.dto.response.RecipeFridgeFoodListsRes;
 import smile.iceBulterrecipe.recipe.dto.request.PostRecipeReq;
 import smile.iceBulterrecipe.recipe.dto.response.BookmarkRes;
+import smile.iceBulterrecipe.recipe.dto.response.MyRecipeListRes;
 import smile.iceBulterrecipe.recipe.dto.response.RecipeListRes;
 import smile.iceBulterrecipe.recipe.dto.response.RecipeRes;
 
@@ -15,4 +16,5 @@ public interface RecipeService{
     BookmarkRes bookmarkRecipe(Long recipeIdx, Long userIdx);
 
     void postRecipe(PostRecipeReq recipeReq, Long userIdx);
+    MyRecipeListRes getMyRecipe(Long userIdx);
 }
