d1763e8974784b1866e2f4d22f4c1cd131abcbf2
sojungpp <sophie2458@naver.com>
2023-05-07 15:23:04 +0900

    #25 feat: 마이 레시피 삭제 구현

--- src/main/java/smile/iceBulterrecipe/recipe/service/RecipeService.java
+++ src/main/java/smile/iceBulterrecipe/recipe/service/RecipeService.java
@@ -17,4 +17,6 @@ public interface RecipeService{
 
     void postRecipe(PostRecipeReq recipeReq, Long userIdx);
     MyRecipeListRes getMyRecipe(Long userIdx);
+
+    void deleteMyRecipe(Long recipeIdx, Long userIdx);
 }
