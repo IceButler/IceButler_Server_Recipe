90c34bccedc3138d7f067a7908704f4cf5e01845
sojungpp <sophie2458@naver.com>
2023-05-11 00:53:10 +0900

    #60 feat: 큐로 전달 받은 식품 추가

--- src/main/java/smile/iceBulterrecipe/food/dto/request/FoodReq.java
+++ src/main/java/smile/iceBulterrecipe/food/dto/request/FoodReq.java
@@ -1,6 +1,10 @@
 package smile.iceBulterrecipe.food.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
+import smile.iceBulterrecipe.global.sqs.FoodData;
+
+import java.util.UUID;
 
 @Getter
 public class FoodReq {
@@ -8,4 +12,22 @@ public class FoodReq {
     private String foodName;
     private String foodImgKey;
     private String foodCategory;
+    private UUID uuid;
+
+    @Builder
+    public FoodReq(String foodName, String foodImgKey, String foodCategory, UUID uuid) {
+        this.foodName = foodName;
+        this.foodImgKey = foodImgKey;
+        this.foodCategory = foodCategory;
+        this.uuid = uuid;
+    }
+
+    public static FoodReq toEntity(FoodData foodData) {
+        return FoodReq.builder()
+                .foodName(foodData.getFoodName())
+                .foodImgKey(foodData.getFoodImgKey())
+                .foodCategory(foodData.getFoodCategory())
+                .uuid(foodData.getUuid())
+                .build();
+    }
 }
--- src/main/java/smile/iceBulterrecipe/global/sqs/AwsSqsListener.java
+++ src/main/java/smile/iceBulterrecipe/global/sqs/AwsSqsListener.java
@@ -1,8 +1,7 @@
 package smile.iceBulterrecipe.global.sqs;
 
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.Message;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.messaging.listener.Acknowledgment;
 import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
 import io.awspring.cloud.messaging.listener.annotation.SqsListener;
@@ -12,27 +11,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import smile.iceBulterrecipe.food.dto.request.FoodReq;
+import smile.iceBulterrecipe.food.service.FoodServiceImpl;
 
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AwsSqsListener {
+
+	private final ObjectMapper objectMapper;
+	private final FoodServiceImpl foodService;
+
 	@Value("${aws.sqs.queue.url}")
 	private String queueUrl;
 
 	@SqsListener(value = "${aws.sqs.queue.name}", deletionPolicy = SqsMessageDeletionPolicy.NEVER)
-	public void listen(@Payload String info, @Headers Map<String, String> headers, Acknowledgment ack) {
+	public void listen(@Payload String info, @Headers Map<String, String> headers, Acknowledgment ack) throws JsonProcessingException {
 		log.info("-------------------------------------start SqsListener");
 		log.info("-------------------------------------info {}", info);
-//		log.info("-------------------------------------headers {}", headers);
-		String id = headers.get("id");
-		System.out.println("id = " + id);
-		long approximateReceiveCount = Long.parseLong(headers.get("ApproximateReceiveCount"));
-		System.out.println("approximateReceiveCount = " + approximateReceiveCount);
 
+		// TODO 메인 서버와 레시피 서버 중 한 곳에서만 listen 되는 문제
+		FoodData foodData = objectMapper.readValue(info, FoodData.class);
+		foodService.addFood(FoodReq.toEntity(foodData));
 
 		ack.acknowledge();
 	}
