2847deb831354f6a3a3e22271d91fbb0318a21cf
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-05-16 20:29:12 +0900

    #23 refactor: RecipeDetailRes -> foodName, recipeImgUrl 추가

--- src/main/java/smile/iceBulterrecipe/recipe/dto/response/RecipeDetailsRes.java
+++ src/main/java/smile/iceBulterrecipe/recipe/dto/response/RecipeDetailsRes.java
@@ -2,6 +2,7 @@ package smile.iceBulterrecipe.recipe.dto.response;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import smile.iceBulterrecipe.global.utils.AwsS3ImageUrlUtils;
 import smile.iceBulterrecipe.recipe.entity.Cookery;
 import smile.iceBulterrecipe.recipe.entity.Recipe;
 import smile.iceBulterrecipe.recipe.entity.RecipeFood;
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 @Data
 @RequiredArgsConstructor
 public class RecipeDetailsRes {
+  private String recipeImgUrl;
   private String recipeName;
   private String recipeCategory;
   private int quantity;
@@ -22,11 +24,12 @@ public class RecipeDetailsRes {
 
   public static RecipeDetailsRes toDto(Recipe recipe, List<RecipeFood> recipeFoods, List<Cookery> cookery) {
     RecipeDetailsRes recipeDetailsRes = new RecipeDetailsRes();
+    recipeDetailsRes.recipeImgUrl = AwsS3ImageUrlUtils.toUrl(recipe.getRecipeImgKey());
     recipeDetailsRes.recipeName = recipe.getRecipeName();
     recipeDetailsRes.recipeCategory = recipe.getRecipeCategory().getCategory();
     recipeDetailsRes.quantity = recipe.getQuantity();
     recipeDetailsRes.leadTime = recipe.getLeadTime();
-    recipeDetailsRes.recipeFoods = recipeFoods.stream().map(m -> RecipeFoodRes.toDto(m.getFoodDetail())).collect(Collectors.toList());
+    recipeDetailsRes.recipeFoods = recipeFoods.stream().map(m -> RecipeFoodRes.toDto(m.getFoodDetail(), m.getFood().getFoodName())).collect(Collectors.toList());
     recipeDetailsRes.cookery = cookery.stream().map(m -> CookeryRes.toDto(m.getDescription(), m.getCookeryImgKey(), m.getNextIdx())).collect(Collectors.toList());
     return recipeDetailsRes;
   }
--- src/main/java/smile/iceBulterrecipe/recipe/dto/response/RecipeFoodRes.java
+++ src/main/java/smile/iceBulterrecipe/recipe/dto/response/RecipeFoodRes.java
@@ -8,15 +8,18 @@ import smile.iceBulterrecipe.recipe.entity.RecipeFood;
 @Data
 @RequiredArgsConstructor
 public class RecipeFoodRes {
+  private String foodName;
   private String foodDetail;
 
   @Builder
-  public RecipeFoodRes(String foodDetail) {
+  public RecipeFoodRes(String foodName, String foodDetail) {
+    this.foodName = foodName;
     this.foodDetail = foodDetail;
   }
 
-  public static RecipeFoodRes toDto(String foodDetail) {
+  public static RecipeFoodRes toDto(String foodDetail, String foodName) {
     return RecipeFoodRes.builder()
+            .foodName(foodName)
             .foodDetail(foodDetail)
             .build();
   }
