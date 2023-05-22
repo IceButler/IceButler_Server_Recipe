97def890e4131af5b75d847acd108765ca277728
이승학 <iseunghag@iseunghag-ui-MacBookAir.local>
2023-05-22 16:54:34 +0900

    #87 feat:어드민 어노테이션 추가

---
+++ src/main/java/smile/iceBulterrecipe/global/resolver/Admin.java
@@ -0,0 +1,12 @@
+package smile.iceBulterrecipe.global.resolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Admin {
+    boolean optional() default false;
+}
---
+++ src/main/java/smile/iceBulterrecipe/global/resolver/AdminLoginStatus.java
@@ -0,0 +1,19 @@
+package smile.iceBulterrecipe.global.resolver;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class AdminLoginStatus {
+    private Boolean isLogin;
+    private Long adminIdx;
+    @Builder
+    public AdminLoginStatus(Boolean isLogin, Long adminIdx) {
+        this.isLogin = isLogin;
+        this.adminIdx = adminIdx;
+    }
+    public static AdminLoginStatus getNotAdminLoginStatus() {
+        return new AdminLoginStatus(false, null);
+    }
+}
