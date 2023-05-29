b0ac180e5f635287a89ba64f46c396b9f1d99e81
chaerlo127 <monynony0203@gmail.com>
2023-05-29 17:44:48 +0900

    #23 fix: 레시피 음식 검색 페이징 에러 수정

--- src/main/java/smile/iceBulterrecipe/recipe/repository/recipe/RecipeRepositoryImpl.java
+++ src/main/java/smile/iceBulterrecipe/recipe/repository/recipe/RecipeRepositoryImpl.java
@@ -1,11 +1,11 @@
 package smile.iceBulterrecipe.recipe.repository.recipe;
 
-import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 import smile.iceBulterrecipe.recipe.entity.Recipe;
 
 import java.util.List;
@@ -20,7 +20,7 @@ public class RecipeRepositoryImpl implements RecipeCustom {
 
     @Override
     public Page<Recipe> findByFoodNameContainingAndIsEnableHavingRecipe(String keyword, Pageable pageable) {
-        List<Recipe> fetch = this.jpaQueryFactory.selectFrom(recipe)
+        QueryResults<Recipe> fetch = this.jpaQueryFactory.selectFrom(recipe)
                 .leftJoin(recipeFood).on(recipe.eq(recipeFood.recipe))
                 .where(recipe.isEnable.eq(true)
                         .and(recipeFood.isEnable.eq(true))
@@ -28,17 +28,11 @@ public class RecipeRepositoryImpl implements RecipeCustom {
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .groupBy(recipe)
-                .fetch();
+                .fetchResults();
 
+        List<Recipe> content = fetch.getResults();
+        long total = fetch.getTotal();
 
-        JPAQuery<Long> count = jpaQueryFactory.select(recipe.count())
-                .from(recipe)
-                .leftJoin(recipeFood).on(recipe.eq(recipeFood.recipe))
-                .where(recipe.isEnable.eq(true)
-                        .and(recipeFood.isEnable.eq(true))
-                        .and(recipeFood.food.foodName.contains(keyword)))
-                .groupBy(recipe);
-
-        return PageableExecutionUtils.getPage(fetch, pageable, count::fetchOne);
+        return new PageImpl<>(content, pageable, total);
     }
 }
